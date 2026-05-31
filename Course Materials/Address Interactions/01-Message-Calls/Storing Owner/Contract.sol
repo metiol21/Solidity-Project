5b41b1db31529a89d83398fffa0a3287f91ad55b
metiol21 <metiolalemayehu12@gmail.com>
2026-05-31 19:46:36 +0300

    Update Contract.sol

--- Course Materials/Address Interactions/01-Message-Calls/Storing Owner/Contract.sol
+++ Course Materials/Address Interactions/01-Message-Calls/Storing Owner/Contract.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Contract {
+
+    address public charity;
+
+    constructor(address _charity) {
+        charity = _charity;
+    }
+
+    receive() external payable {}
+
+    function donate() public {
+        selfdestruct(payable(charity));
+    }
+}
