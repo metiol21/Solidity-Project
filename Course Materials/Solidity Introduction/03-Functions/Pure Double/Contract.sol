70bde32ad0925b2ac708ce3bb69fc710b64ce31e
metiol21 <metiolalemayehu12@gmail.com>
2026-05-31 19:34:24 +0300

    Update Contract.sol

--- Course Materials/Solidity Introduction/03-Functions/Pure Double/Contract.sol
+++ Course Materials/Solidity Introduction/03-Functions/Pure Double/Contract.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Contract {
+
+    // single-argument version (from previous lesson)
+    function double(uint x) public pure returns (uint) {
+        return x * 2;
+    }
+
+    // overloaded version (THIS is the new requirement)
+    function double(uint x, uint y) external pure returns (uint, uint) {
+        return (x * 2, y * 2);
+    }
+}
