297ac421cdc52cc3b5fb60d39336138660df6e73
metiol21 <metiolalemayehu12@gmail.com>
2026-05-31 19:48:36 +0300

    Update Contract.sol

--- Course Materials/Address Interactions/02-Reverting-Transactions/Constructor Revert/Contract.sol
+++ Course Materials/Address Interactions/02-Reverting-Transactions/Constructor Revert/Contract.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Contract {
+
+    address public owner;
+
+    constructor() payable {
+        require(msg.value >= 1 ether, "Need 1 ether");
+        owner = msg.sender;
+    }
+
+    function withdraw() public {
+        require(msg.sender == owner, "Not owner");
+        payable(owner).transfer(address(this).balance);
+    }
+}
